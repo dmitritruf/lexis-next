82e64976c2063568b8f5b6b0074ec9c7572e161e
dmitritruf <gttim2@gmail.com>
2022-02-26 12:50:31 -0300

    feat: structure _app.jsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app'
 
-import '../styles/globals.css'
+import styles from '../styles/app.module.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <section className={ styles.wrapper }>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </section>
+  )
+  
 }
 
 export default MyApp
