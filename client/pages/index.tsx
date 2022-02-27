962a67dfcd52384171ff6221bbf39965c1db357c
dmitritruf <gttim2@gmail.com>
2022-02-27 10:36:37 -0300

    feat: create HeaderMenu instance

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,13 @@
 import { Carousel } from '../components/Carousel'
+import { HeaderMenu } from '../components/HeaderMenu'
+
 import styles from './home.module.scss'
 
 export default function Home() : JSX.Element {
   return (
     <div className={ styles.homeWrapper }>
       <Carousel />
+      <HeaderMenu />
     </div>
   )
 }
